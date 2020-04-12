7461e07aded274925a49f0d3dc091122b2b4e9e7
cindyadonia <cindy.adonia3@gmail.com>
2020-04-12 20:36:30 +0700

    Handle interval and update refresh chat when receive file

--- application/views/message/index.php
+++ application/views/message/index.php
@@ -70,103 +70,182 @@
 
     <!--This page JavaScript -->
     <script>
-        $('.get-chat-box').on('click',function(){
-			var recipient_no = $(this).attr('recipient-no');
-            $('#file-recipient-no').val(recipient_no);
-            $.ajax({
-                type:'POST',
-                url:'<?php echo base_url("Message/showChat/"); ?>' + recipient_no,
-                data:{},
-                success:function(data){
-                    $('.right-part').empty();
-                    var jsonObject = JSON.parse(data);
-					var html = "";
-                    html += "<div class='p-20'>";
-                    html += "<div class='card'>";
-                    html += "<div class='card-body'>";
-                    html += "<h4 class='card-title'>" + jsonObject.receiver['name'] + "</h4>";
-                    html += "<div class='chat-box scrollable' style='height:calc(100vh - 300px);'>";
-                    html += "<ul class='chat-list'>";
-					for (i = 0; i < jsonObject.chats.length; i++)
-					{
-						if(jsonObject.chats[i].sender_no == <?= $this->session->userdata('user_no') ?>)
-						{
-							html += "<li class='odd chat-item' data-id='" + jsonObject.chats[i].id + "'>";
-							html += "<div class='chat-content'>";
-                            if (jsonObject.chats[i].file_path) {
-                                html += "<div class='box bg-light-inverse'>File has been sent. <a style='color:Yellow' href='<?php echo base_url()?>Message/download/" + jsonObject.chats[i].file_path+ "'>Download here</a></div>";
-                            } else {
-							    html += "<div class='box bg-light-inverse'>" + jsonObject.chats[i].content + "</div>";
-                            }
-							html += "</div>";
-							html += "<div class='chat-time'>" +new Date(jsonObject.chats[i].created_at).toString().slice(16,21); + "</div>";
-							html += "</li>";
-						}
-						else if(jsonObject.chats[i].sender_no != <?= $this->session->userdata('user_no') ?>)
-						{
-							html += "<li class='chat-item' data-id='" + jsonObject.chats[i].id + "'>";
-							html += "<div class='chat-content'>";
-                            if (jsonObject.chats[i].file_path) {
-                                html += "<div class='box bg-light-info'>File has been sent. <a href='<?php echo base_url()?>Message/download/" + jsonObject.chats[i].file_path+ "'>Download here</a></div>";
-                            } else {
-							    html += "<div class='box bg-light-info'>" + jsonObject.chats[i].content + "</div>";
-                            }
-							html += "</div>";
-							html += "<div class='chat-time'>" +new Date(jsonObject.chats[i].created_at).toString().slice(16,21); + "</div>";
-							html += "</li>";
-						}
-					}
-					html += "</ul>";
-					html += "</div>";
-					html += "</div>";
-					html += "<div class='card-body border-top'>"
-					html += "<div class='row'>"
-					html += "<div class='col-9'>";
-					html += "<div class='input-field m-t-0 m-b-0'>";
-					html += "<input id='message' placeholder='Type and enter' class='form-control border-0' type='text'>";
-					html += "</div>";
-					html += "</div>";
-					html += "<div class='col-3'>";
-					html += "<a id='choose-file' class='btn-circle btn-lg btn-cyan float-right text-white'><i class='fas fa-paperclip'></i></a>";
-					html += "<a id='send-msg' class='btn-circle btn-lg btn-cyan float-right text-white' href='javascript:void(0)'><i class='fas fa-paper-plane'></i></a>";
-                    html += "</div>";
-                    html += "</div>";
-					html += "</div>";
-					html += "</div>";
-                    html += "</div>";
-                    $('.right-part').append(html);
+        class MyInterval {
+            status = false;
+            constructor(status) {
+                this.status = status;
+            }
 
-                    var input = document.getElementById("message");
-                    input.addEventListener("keyup", function(event) {
-                        if (event.keyCode === 13) {
-                            event.preventDefault();
-                            document.getElementById("send-msg").click();
-                        }
-                    });
+            getStatus() { return this.status; };
+            setStatus(status) { this.status = status; };
+        }
+        var currentInterval = false;
+        $(document).ready(function() {
+            var refreshInterval = new MyInterval(false);
+            function Interval(fn, time) {
+                var timer = false;
+                this.start = function () {
+                    if (!this.isRunning())
+                        console.log("call start");
+                        console.log(fn, time);
+                        timer = setInterval(fn, time);
+                };
+                this.stop = function () {
+                    console.log("call stop")
+                    clearInterval(timer);
+                    timer = false;
+                };
+                this.isRunning = function () {
+                    console.log("Check is running");
+                    return timer !== false;
+                };
+            }
 
-                    $('#send-msg').on('click', function(){			
-                        var msg = $(this).parent().prev().children().children().val();
-                        if(msg){
-                            $.ajax({
-                                type:'POST',
-                                url:'<?php echo base_url("Message/store/"); ?>',
-                                data:{
-                                    message:msg,
-                                    recipient:recipient_no,
-                                },
-                                success:function(data){
-                                    $('#message').val("");
+            function refreshChat(recipient_no)
+            {
+                console.log(recipient_no);
+                var last_chat_id = $('ul.chat-list li:last-child').attr('data-id');
+                $.ajax({
+                    type:'POST',
+                    url:'<?php echo base_url("Message/newMessage/"); ?>' + recipient_no + "/" + last_chat_id,
+                    data:{},
+                    success:function(data){
+                        var jsonObject = JSON.parse(data);
+                        var html = "";
+                        for (i = 0; i < jsonObject.new_chats.length; i++)
+                        {
+                            if(jsonObject.new_chats[i].sender_no == <?= $this->session->userdata('user_no') ?>)
+                            {
+                                html += "<li class='odd chat-item' data-id='" + jsonObject.new_chats[i].id + "'>";
+                                html += "<div class='chat-content'>";
+                                if (jsonObject.new_chats[i].file_path) {
+                                    html += "<div class='box bg-light-inverse'>File has been sent. <a style='color:Yellow' href='<?php echo base_url()?>Message/download/" + jsonObject.new_chats[i].file_path+ "'>Download here</a></div>";
+                                } else {
+                                    html += "<div class='box bg-light-inverse'>" + jsonObject.new_chats[i].content + "</div>";
                                 }
-                            });
+                                // html += "<div class='box bg-light-inverse'>" + jsonObject.new_chats[i].content + "</div>";
+                                // html += "</div>";
+                                html += "<div class='chat-time'>" +new Date(jsonObject.new_chats[i].created_at).toString().slice(16,21); + "</div>";
+                                html += "</li>";
+                            }
+                            else if(jsonObject.new_chats[i].sender_no != <?= $this->session->userdata('user_no') ?>)
+                            {
+                                html += "<li class='chat-item' data-id='" + jsonObject.new_chats[i].id + "'>";
+                                html += "<div class='chat-content'>";
+                                if (jsonObject.new_chats[i].file_path) {
+                                    html += "<div class='box bg-light-info'>File has been sent. <a href='<?php echo base_url()?>Message/download/" + jsonObject.new_chats[i].file_path+ "'>Download here</a></div>";
+                                } else {
+                                    html += "<div class='box bg-light-info'>" + jsonObject.new_chats[i].content + "</div>";
+                                }
+                                // html += "<div class='box bg-light-info'>" + jsonObject.new_chats[i].content + "</div>";
+                                // html += "</div>";
+                                html += "<div class='chat-time'>" +new Date(jsonObject.new_chats[i].created_at).toString().slice(16,21); + "</div>";
+                                html += "</li>";
+                            }
                         }
-                    });
-                }
-            });
+                        $('ul.chat-list').append(html);
+                        $('.chat-box.scrollable').scrollTop($('.chat-list').height());
+                    }
+                }); 
+            }
 
-            setInterval(function(){
-                refreshChat(recipient_no);
-            }, 2000);
-    	});
+            $('.get-chat-box').on('click',function(){
+                var recipient_no = $(this).attr('recipient-no');
+                $('#file-recipient-no').val(recipient_no);
+                $.ajax({
+                    type:'POST',
+                    url:'<?php echo base_url("Message/showChat/"); ?>' + recipient_no,
+                    data:{},
+                    success:function(data){
+                        $('.right-part').empty();
+                        var jsonObject = JSON.parse(data);
+                        var html = "";
+                        html += "<div class='p-20'><div class='card'><div class='card-body'>";
+                        html += "<h4 class='card-title'>" + jsonObject.receiver['name'] + "</h4>";
+                        html += "<div class='chat-box scrollable' style='height:calc(100vh - 300px);'>";
+                        html += "<ul class='chat-list'>";
+                        for (i = 0; i < jsonObject.chats.length; i++)
+                        {
+                            if(jsonObject.chats[i].sender_no == <?= $this->session->userdata('user_no') ?>)
+                            {
+                                html += "<li class='odd chat-item' data-id='" + jsonObject.chats[i].id + "'>";
+                                html += "<div class='chat-content'>";
+                                if (jsonObject.chats[i].file_path) {
+                                    html += "<div class='box bg-light-inverse'>File has been sent. <a style='color:Yellow' href='<?php echo base_url()?>Message/download/" + jsonObject.chats[i].file_path+ "'>Download here</a></div>";
+                                } else {
+                                    html += "<div class='box bg-light-inverse'>" + jsonObject.chats[i].content + "</div>";
+                                }
+                                html += "</div>";
+                                html += "<div class='chat-time'>" +new Date(jsonObject.chats[i].created_at).toString().slice(16,21); + "</div>";
+                                html += "</li>";
+                            }
+                            else if(jsonObject.chats[i].sender_no != <?= $this->session->userdata('user_no') ?>)
+                            {
+                                html += "<li class='chat-item' data-id='" + jsonObject.chats[i].id + "'>";
+                                html += "<div class='chat-content'>";
+                                if (jsonObject.chats[i].file_path) {
+                                    html += "<div class='box bg-light-info'>File has been sent. <a href='<?php echo base_url()?>Message/download/" + jsonObject.chats[i].file_path+ "'>Download here</a></div>";
+                                } else {
+                                    html += "<div class='box bg-light-info'>" + jsonObject.chats[i].content + "</div>";
+                                }
+                                html += "</div>";
+                                html += "<div class='chat-time'>" +new Date(jsonObject.chats[i].created_at).toString().slice(16,21); + "</div>";
+                                html += "</li>";
+                            }
+                        }
+                        html += "</ul></div></div><div class='card-body border-top'><div class='row'><div class='col-9'><div class='input-field m-t-0 m-b-0'><input id='message' placeholder='Type and enter' class='form-control border-0' type='text'></div></div><div class='col-3'><a id='choose-file' class='btn-circle btn-lg btn-cyan float-right text-white'><i class='fas fa-paperclip'></i></a><a id='send-msg' class='btn-circle btn-lg btn-cyan float-right text-white' href='javascript:void(0)'><i class='fas fa-paper-plane'></i></a></div></div></div></div></div>";
+                        
+                        $('.right-part').append(html);
+                        // console.log($('.chat-box.scrollable').scrollHeight);
+                        $('.chat-box.scrollable').scrollTop($('.chat-list').height());
+
+                        var input = document.getElementById("message");
+                        input.addEventListener("keyup", function(event) {
+                            if (event.keyCode === 13) {
+                                event.preventDefault();
+                                document.getElementById("send-msg").click();
+                            }
+                        });
+
+                        $('#send-msg').on('click', function(){			
+                            var msg = $(this).parent().prev().children().children().val();
+                            if(msg){
+                                $.ajax({
+                                    type:'POST',
+                                    url:'<?php echo base_url("Message/store/"); ?>',
+                                    data:{
+                                        message:msg,
+                                        recipient:recipient_no,
+                                    },
+                                    success:function(data){
+                                        $('#message').val("");
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
+
+                if (refreshInterval.getStatus()) {
+                    currentInterval.stop();
+                    
+                    currentInterval = new Interval(function() {
+                        refreshChat(recipient_no)
+                    }, 1000);
+                    currentInterval.start();
+                    refreshInterval.setStatus(true);
+                } else {
+                    currentInterval = new Interval(function() {
+                        refreshChat(recipient_no)
+                    }, 1000);
+                    currentInterval.start();
+                    refreshInterval.setStatus(true);
+                }
+                
+            });
+        });
+
+
 
         $(document).ready(function() {
             $(document).on("click", "#choose-file", function () {
@@ -192,43 +271,5 @@
                 });
             });
         });
-
-
-        function refreshChat(recipient_no)
-        {
-            var last_chat_id = $('ul.chat-list li:last-child').attr('data-id');
-			$.ajax({
-                type:'POST',
-                url:'<?php echo base_url("Message/newMessage/"); ?>' + recipient_no + "/" + last_chat_id,
-                data:{},
-                success:function(data){
-                    var jsonObject = JSON.parse(data);
-					var html = "";
-                    for (i = 0; i < jsonObject.new_chats.length; i++)
-					{
-						if(jsonObject.new_chats[i].sender_no == <?= $this->session->userdata('user_no') ?>)
-						{
-							html += "<li class='odd chat-item' data-id='" + jsonObject.new_chats[i].id + "'>";
-							html += "<div class='chat-content'>";
-							html += "<div class='box bg-light-inverse'>" + jsonObject.new_chats[i].content + "</div>";
-							html += "</div>";
-							html += "<div class='chat-time'>" +new Date(jsonObject.new_chats[i].created_at).toString().slice(16,21); + "</div>";
-							html += "</li>";
-						}
-						else if(jsonObject.new_chats[i].sender_no != <?= $this->session->userdata('user_no') ?>)
-						{
-							html += "<li class='chat-item' data-id='" + jsonObject.new_chats[i].id + "'>";
-							html += "<div class='chat-content'>";
-							html += "<div class='box bg-light-info'>" + jsonObject.new_chats[i].content + "</div>";
-							html += "</div>";
-							html += "<div class='chat-time'>" +new Date(jsonObject.new_chats[i].created_at).toString().slice(16,21); + "</div>";
-							html += "</li>";
-						}
-					}
-                    $('ul.chat-list').append(html);
-                }
-            }); 
-        }
-
     </script>
 </body>
